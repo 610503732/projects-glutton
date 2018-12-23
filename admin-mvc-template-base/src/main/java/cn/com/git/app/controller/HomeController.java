0f5e38b54bec6e44ea7e5882926532268da59c3b
Administrator <610503732@>
2018-12-23 16:49:49 +0800

    admin-mvc-template-base mysql

--- admin-mvc-template-base/src/main/java/cn/com/git/app/controller/HomeController.java
+++ admin-mvc-template-base/src/main/java/cn/com/git/app/controller/HomeController.java
@@ -1,10 +1,15 @@
 package cn.com.git.app.controller;
 
+import cn.com.git.app.dao.SysUserMapper;
+import cn.com.git.app.entity.pojo.SysUser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 
 /**
@@ -19,10 +24,14 @@ public class HomeController {
     @Value("${server.env.name}")
     private String serverEnvName ;
 
+    @Autowired
+    private SysUserMapper sysUserMapper ;
+
     @RequestMapping("/index")
     public String index(String name) {
         logger.info("info:{}","主页 index ") ;
         logger.info("info:当前环境 {}",serverEnvName) ;
+        SysUser list =  sysUserMapper.select();
         return "home/index" ;
     }
 
---
+++ admin-mvc-template-base/src/main/java/cn/com/git/app/dao/SysUserMapper.java
@@ -0,0 +1,11 @@
+package cn.com.git.app.dao;
+
+import cn.com.git.app.entity.pojo.SysUser;
+
+import java.util.List;
+
+public interface SysUserMapper {
+
+    SysUser select();
+
+}
