5a71988e0e034ebcda3ff68080dd6f1574de7987
Administrator <610503732@>
2018-12-23 10:55:56 +0800

    admin-mvc-template-base

--- admin-mvc-template-base/src/main/java/cn/com/git/app/controller/HomeController.java
+++ admin-mvc-template-base/src/main/java/cn/com/git/app/controller/HomeController.java
@@ -2,6 +2,7 @@ package cn.com.git.app.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -15,10 +16,13 @@ public class HomeController {
 
     private static Logger logger = LoggerFactory.getLogger(HomeController.class);// slf4j日志记录器
 
+    @Value("${server.env.name}")
+    private String serverEnvName ;
+
     @RequestMapping("/index")
     public String index(String name) {
         logger.info("info:{}","主页 index ") ;
-
+        logger.info("info:当前环境 {}",serverEnvName) ;
         return "home/index" ;
     }
 
